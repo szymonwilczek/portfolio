039886fbe0a1e161cd6a9bf7c479ab62b65508f6
Szymon Wilczek <szymonwilczek@outlook.com>
2025-12-09 03:02:29 +0100

    fix: removed disabling transition on change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="system"
           enableSystem
-          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
